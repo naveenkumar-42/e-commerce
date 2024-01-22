631c1300acced02b94c8cc403d809150c8158a82
naveenkumar-42 <naveenkumarpoff@gmail.com>
2024-01-22 17:08:32 +0530

    updated

--- menspage/mens.js
+++ menspage/mens.js
@@ -1,4 +1,73 @@
-var productData = [{
+var productData = [
+    
+     {
+    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13640716/2021/5/24/48e9e3bc-8fb7-4af5-97fa-46927f68fbd81621838454680JeansRoadsterMenJeansRoadsterMenTrackPantsHRXbyHrithikRoshan1.jpg ",
+    brand: "U.S. Polo Assn.",
+    para: "Printed Polo Collar Pure Cotton T-Shirt",
+    price: "Rs. 1259",
+    rs: 1259,
+    strikedoffprice: "Rs 2099",
+    offer: "(40% OFF)",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "Tshirts",
+    }, {
+    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11359590/2020/2/26/68da9e40-e8b1-4710-8282-802c2c6550c11582709189210-US-Polo-Assn-Men-Tshirts-9481582709187568-1.jpg ",
+    brand: "HRX by Hrithik Roshan",
+    para: "Stiped Polo Collar Pur Cotton T-Shirt ",
+    price: "Rs. 949 ",
+    rs: 949,
+    strikedoffprice: "Rs 1899 ",
+    offer: "(50% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "Tshirts",
+   },
+     {
+    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11359600/2020/6/22/97e8ab74-25ff-4daf-bffb-b0a1ec708c191592819068194ShirtsUSPoloAssnMenShirtsUSPoloAssnMenShirtsUSPoloAssnMenShi1.jpg ",
+    brand: "PUMA",
+    para: "Colourblocked Polo Collar Pure Cotton ",
+    price: "Rs. 1139 ",
+    rs: 1139,
+    strikedoffprice: "Rs 1899 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "Tshirts",
+    }, {
+    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/1/17/8088e3c4-5737-482b-80de-8d9bcede75f81579215077796-1.jpg ",
+    brand: "Flying Machine",
+    para: "Printed Polo Collar Pure Cotton T-Shirt ",
+    price: "Rs. 1259 ",
+    rs: 1259,
+    strikedoffprice: "Rs 2099 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "Tshirts",
+},{
+    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13640688/2021/4/17/28b7b7ea-6a87-42df-9577-4299f5030a411618637780329-US-Polo-Assn-Men-Tshirts-7061618637779314-1.jpg ",
+    brand: "PUMA",
+    para: "Slim Fit Striped Pure Cotton T-Shirt",
+    price: "Rs. 1299",
+    rs: 1299,
+    strikedoffprice: "Rs 1999",
+    offer: "(35% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "Slippers",
+}, {
+    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1924785/2018/5/18/be8485c7-0944-4f38-8f5f-78b6e325fd201526634385328-US-Polo-Assn-Men-Khaki-Slim-Fit-Solid-Chinos-7181526634383854-1.jpg ",
+    brand: "WROGN",
+    para: "Men Slim Fit Chinos ",
+    price: "Rs. 1049 ",
+    rs: 1049,
+    strikedoffprice: "Rs 2099 ",
+    offer: "(50% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "Trousers"
+    },{
     image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/10331501/2020/3/18/d4d07c60-88d9-43d9-aa78-9cc7712816321584508934272-US-Polo-Assn-Men-White-Colourblocked-Sneakers-84015845089331-1.jpg",
     brand: "U.S. Polo Assn.",
     para: "Men Colourblocked Sneakers",
@@ -10,7 +79,9 @@ var productData = [{
     atw: "Add to Whishlist",
     category: "Casual Shoes",
 
-}, {
+},
+    
+    {
     image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13031344/2021/2/15/edb298bb-2c55-4b17-be70-b99fa7da98af1613389238510-US-Polo-Assn-Men-Navy-Blue-Printed-Sliders-7711613389237151-1.jpg ",
     brand: "U.S. Polo Assn.",
     para: "Men Printed Sliders",
@@ -21,17 +92,6 @@ var productData = [{
     atc: "Add to Bag",
     atw: "Add to Whishlist",
     category: "Slippers",
-}, {
-    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13640716/2021/5/24/48e9e3bc-8fb7-4af5-97fa-46927f68fbd81621838454680JeansRoadsterMenJeansRoadsterMenTrackPantsHRXbyHrithikRoshan1.jpg ",
-    brand: "U.S. Polo Assn.",
-    para: "Printed Polo Collar Pure Cotton T-Shirt",
-    price: "Rs. 1259",
-    rs: 1259,
-    strikedoffprice: "Rs 2099",
-    offer: "(40% OFF)",
-    atc: "Add to Bag",
-    atw: "Add to Whishlist",
-    category: "Tshirts",
 }, {
     image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/10339033/2019/8/6/df9a712b-a5fc-4a65-870a-0854fb50ea221565093150139-US-Polo-Assn-Men-Casual-Shoes-7391565093148433-1.jpg ",
     brand: "U.S. Polo Assn.",
@@ -176,28 +236,6 @@ var productData = [{
     atw: "Add to Whishlist",
     category: "Flip Flops",
 }, {
-    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11359600/2020/6/22/97e8ab74-25ff-4daf-bffb-b0a1ec708c191592819068194ShirtsUSPoloAssnMenShirtsUSPoloAssnMenShirtsUSPoloAssnMenShi1.jpg ",
-    brand: "PUMA",
-    para: "Colourblocked Polo Collar Pure Cotton ",
-    price: "Rs. 1139 ",
-    rs: 1139,
-    strikedoffprice: "Rs 1899 ",
-    offer: "(40% OFF) ",
-    atc: "Add to Bag",
-    atw: "Add to Whishlist",
-    category: "Tshirts",
-}, {
-    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1924785/2018/5/18/be8485c7-0944-4f38-8f5f-78b6e325fd201526634385328-US-Polo-Assn-Men-Khaki-Slim-Fit-Solid-Chinos-7181526634383854-1.jpg ",
-    brand: "WROGN",
-    para: "Men Slim Fit Chinos ",
-    price: "Rs. 1049 ",
-    rs: 1049,
-    strikedoffprice: "Rs 2099 ",
-    offer: "(50% OFF) ",
-    atc: "Add to Bag",
-    atw: "Add to Whishlist",
-    category: "Trousers"
-}, {
     image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15028790/2021/8/13/584feeb0-e6f2-4b12-8549-7a0566f382651628856325445-US-Polo-Assn-Men-Sports-Shoes-4781628856324962-1.jpg ",
     brand: "WROGN",
     para: "Men BRONEL 2.0 Walking Shoe ",
@@ -208,18 +246,7 @@ var productData = [{
     atc: "Add to Bag",
     atw: "Add to Whishlist",
     category: "Casual Shoes",
-}, {
-    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11359590/2020/2/26/68da9e40-e8b1-4710-8282-802c2c6550c11582709189210-US-Polo-Assn-Men-Tshirts-9481582709187568-1.jpg ",
-    brand: "HRX by Hrithik Roshan",
-    para: "Stiped Polo Collar Pur Cotton T-Shirt ",
-    price: "Rs. 949 ",
-    rs: 949,
-    strikedoffprice: "Rs 1899 ",
-    offer: "(50% OFF) ",
-    atc: "Add to Bag",
-    atw: "Add to Whishlist",
-    category: "Tshirts",
-}, {
+},{
     image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14406186/2021/7/13/89fe5eaf-6a09-4695-ab3a-651478bc0e6f1626156204788-US-Polo-Assn-Men-Casual-Shoes-1211626156204359-1.jpg ",
     brand: "HRX by Hrithik Roshan",
     para: "Men Textured Loafers ",
@@ -252,17 +279,6 @@ var productData = [{
     atc: "Add to Bag",
     atw: "Add to Whishlist",
     category: "Slippers",
-}, {
-    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/1/17/8088e3c4-5737-482b-80de-8d9bcede75f81579215077796-1.jpg ",
-    brand: "Flying Machine",
-    para: "Printed Polo Collar Pure Cotton T-Shirt ",
-    price: "Rs. 1259 ",
-    rs: 1259,
-    strikedoffprice: "Rs 2099 ",
-    offer: "(40% OFF) ",
-    atc: "Add to Bag",
-    atw: "Add to Whishlist",
-    category: "Tshirts",
 }, {
     image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15246010/2021/10/13/006a88c9-920e-4ca3-b9e2-96d6410cd2ad1634123262816-US-Polo-Assn-Men-Casual-Shoes-5701634123262500-1.jpg ",
     brand: "Flying Machine",
@@ -318,18 +334,7 @@ var productData = [{
     atc: "Add to Bag",
     atw: "Add to Whishlist",
     category: "Flip Flops",
-}, {
-    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13640688/2021/4/17/28b7b7ea-6a87-42df-9577-4299f5030a411618637780329-US-Polo-Assn-Men-Tshirts-7061618637779314-1.jpg ",
-    brand: "PUMA",
-    para: "Slim Fit Striped Pure Cotton T-Shirt",
-    price: "Rs. 1299",
-    rs: 1299,
-    strikedoffprice: "Rs 1999",
-    offer: "(35% OFF) ",
-    atc: "Add to Bag",
-    atw: "Add to Whishlist",
-    category: "Slippers",
-}, ];
+},  ];
 
 
 var wishListData = JSON.parse(localStorage.getItem("wishListObj")) || []
