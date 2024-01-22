0b010f1fa36ca14144c8de6fb60806f4acf6348d
jeevithakannan2 <ksjeevithakannan123@gmail.com>
2024-01-22 19:10:58 +0530

    Fixed bug - Cart not opening from home page

--- Common Files/hyperlinks.js
+++ Common Files/hyperlinks.js
@@ -10,7 +10,7 @@ document.querySelector(".fa_wishlist").addEventListener('click', function(){
     window.location.href = "../wishlist/wishlist.html"
 })
 document.querySelector('.fa-bag-shopping').addEventListener('click', function(){
-    window.location.href = "../wishlist/cart.html"
+    window.location.href = "./wishlist/cart.html"
 })
 document.querySelector('.fa_cart').addEventListener('click', function(){
     window.location.href = "../wishlist/cart.html"
